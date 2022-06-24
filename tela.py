39c220669c575c13b524c2332c5e65159214cb2b
matheus <matheushfbrandao@gmail.com>
2022-06-24 11:33:46 -0300

    modulo_display_correcao_nome

--- tela.py
+++ tela.py
@@ -1,21 +1,11 @@
-# Complete project details at https://RandomNerdTutorials.com
+import machine, ssd1306
+i2c = machine.I2C(scl=machine.Pin(4), sda=machine.Pin(5))
 
-from machine import Pin, SoftI2C
-import ssd1306
-from time import sleep
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-# ESP32 Pin assignment 
-i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
-
-# ESP8266 Pin assignment
-#i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
-
-oled_width = 128
-oled_height = 64
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-
-oled.text('Hello, World 1!', 0, 0)
-oled.text('Hello, World 2!', 0, 10)
-oled.text('Hello, World 3!', 0, 20)
-        
+oled.fill(0) 
+oled.text('MicroPython on', 0, 0)
+oled.text('an ESP32 with an', 0, 10)
+oled.text('attached SSD1306', 0, 20)
+oled.text('OLED display', 0, 30)
 oled.show()
